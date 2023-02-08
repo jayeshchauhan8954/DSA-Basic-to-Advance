8f0edcdd0993d905b45e9d486c4196c19fee6ec5
jayeshchauhan8954 <officialjitandrachauhan@gmail.com>
2023-02-09 00:06:07 +0530

    Reverse Arrary

--- 8.feb.2023/reverseArray.js
+++ 8.feb.2023/reverseArray.js
@@ -1,10 +1,17 @@
 // reverse the array
 
 function reverseArray(arr) {
-	let left = 0,
-		right = arr.length - 1;
+	let start = 0,
+		end = arr.length - 1;
+	while (start < end) {
+		let temp = arr[start];
+		arr[start] = arr[end];
+		arr[end] = temp;
+		start++;
+		end--;
+	}
+	return arr;
 }
-
 let array = [ 1, 2, 5, 8, 4, 0, 9 ];
 console.log(reverseArray(array));
 // output should be [9,0,4,8,5,2,1];
