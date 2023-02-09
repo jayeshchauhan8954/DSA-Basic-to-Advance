0cb83b09003543fe0dff829260c8980f0f2320b1
jayeshchauhan8954 <officialjitandrachauhan@gmail.com>
2023-02-09 12:51:25 +0530

    find min and max seperately

--- revision/findMinMax.js
+++ revision/findMinMax.js
@@ -1,15 +1,45 @@
 // find minimum of an array
-function findMin(arr) {
+function findMinMap(arr) {
 	let minimum = Math.min(...arr.map((el) => el));
 	return minimum;
 }
 let array = [ 1, 2, 45, 7, 78, 94, 75, 90 ];
-console.log(findMin(array));
+console.log(findMinMap(array));
 
 // find maximum of an array
-function findMax(arr) {
+function findMaxMap(arr) {
 	let maximum = Math.max(...arr.map((el) => el));
 	return maximum;
 }
 let array1 = [ 1, 2, 45, 7, 78, 94, 75, 90 ];
-console.log(findMax(array1));
+console.log(findMaxMap(array1));
+
+// let findMin from normal for loop
+
+function findMin(arr) {
+	let min = arr[0];
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] <= min) {
+			min = arr[i];
+		}
+	}
+	return min;
+}
+
+let arr1 = [ 1, 23, 4, 567, 78, 56, 34 ];
+console.log(findMin(arr1));
+
+// let findMax from normal for loop
+function findMax(arr) {
+	let max = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] > max) {
+			//   console.log(arr[i]);
+			max = arr[i];
+		}
+	}
+	return max;
+}
+
+let arr2 = [ 1, 23, 4, 567, 78, 56, 34 ];
+console.log(findMax(arr2));
